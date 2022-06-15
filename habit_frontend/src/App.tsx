775e008b2b6c5b97a3413da89b0c8f6bda4d9e9c
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-15 16:00:59 +0100

    add home component

--- habit_frontend/src/App.tsx
+++ habit_frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
+import Home from "./components/Home";
 interface Response {
   id: any;
   name: string;
@@ -21,6 +22,7 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <Home />
       <h1> Do it over and over and over again </h1>
       {habits.map((habit, indx) => {
         return <p>{habit.name}</p>;
---
+++ habit_frontend/src/components/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1> Integrate </h1>
+    </div>
+  );
+};
+export default Home;

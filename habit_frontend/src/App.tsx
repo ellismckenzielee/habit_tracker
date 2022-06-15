7334112d05d813a3688cab39657cb6d5faa1c2c4
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-15 16:28:15 +0100

    remove old code from home component

--- habit_frontend/src/App.tsx
+++ habit_frontend/src/App.tsx
@@ -1,32 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
 import "./App.css";
 import Home from "./components/Home";
-interface Response {
-  id: any;
-  name: string;
-}
+import Login from "./components/Login";
+
 function App() {
-  const [habits, setHabits] = useState<any[]>([]);
-  useEffect(() => {
-    const url = "http://localhost:5656/habits";
-    console.log(url);
-    axios
-      .get(url)
-      .then(({ data: habits }: { data: Response }) => {
-        setHabits([habits]);
-      })
-      .catch(() => {
-        console.log("unsuccessful");
-      });
-  }, []);
   return (
     <div className="App">
       <Home />
-      <h1> Do it over and over and over again </h1>
-      {habits.map((habit, indx) => {
-        return <p>{habit.name}</p>;
-      })}
+      <Login />
     </div>
   );
 }

23529f60eb071f0d94a7ed8c0ddc62fdb4d887b1
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-15 17:16:27 +0100

    add home component to browser router

--- habit_frontend/src/App.tsx
+++ habit_frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Home from "./components/Home";
+import Main from "./components/Main";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Main />} />
         </Routes>
       </BrowserRouter>
     </div>

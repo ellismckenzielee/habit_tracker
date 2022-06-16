ea4f2dbbb336eb115bdc7d65caf3efffbfd7a2ea
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-16 16:29:18 +0100

    add view component + stylesheet

--- habit_frontend/src/components/Main.tsx
+++ habit_frontend/src/components/Main.tsx
@@ -1,8 +1,10 @@
 import style from "../styles/Main.module.css";
+import View from "./View";
 const Main = () => {
   return (
     <div className={style.Main}>
       <h1> This is the main page </h1>
+      <View />
     </div>
   );
 };
---
+++ habit_frontend/src/components/View.tsx
@@ -0,0 +1,10 @@
+import style from "../styles/View.module.css";
+const View = () => {
+  return (
+    <div className={style.View}>
+      <p>This is the view</p>
+    </div>
+  );
+};
+
+export default View;

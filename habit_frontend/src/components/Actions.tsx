28bf8bfdd07fc92d45334af306c09b68d8172b48
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-16 16:32:17 +0100

    add actions component + stylesheet

---
+++ habit_frontend/src/components/Actions.tsx
@@ -0,0 +1,11 @@
+import style from "../styles/Actions.module.css";
+
+const Actions = () => {
+  return (
+    <div className={style.Actions}>
+      <p>This is the actions component</p>
+    </div>
+  );
+};
+
+export default Actions;
--- habit_frontend/src/components/Main.tsx
+++ habit_frontend/src/components/Main.tsx
@@ -1,10 +1,12 @@
 import style from "../styles/Main.module.css";
+import Actions from "./Actions";
 import View from "./View";
 const Main = () => {
   return (
     <div className={style.Main}>
       <h1> This is the main page </h1>
       <View />
+      <Actions />
     </div>
   );
 };

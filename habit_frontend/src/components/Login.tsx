a7a513336f11617c6ad0db9e150f93d6f8aa17e4
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-21 16:59:46 +0100

    add in successful login functionality and hardcode habits based on id

--- habit_frontend/src/components/Login.tsx
+++ habit_frontend/src/components/Login.tsx
@@ -1,8 +1,13 @@
 import style from "../styles/Login.module.css";
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 const Login = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(password);
   return (
     <div className={style.Login}>
       {isLoggedIn && <Navigate to="/profile" />}
@@ -13,13 +18,34 @@ const Login = () => {
           className={style.Form}
           onSubmit={(e) => {
             e.preventDefault();
-            setIsLoggedIn(true);
+            axios
+              .post("http://localhost:5656/user/login", { username, password })
+              .then(({ data }) => {
+                setUser(data);
+                setIsLoggedIn(true);
+              });
           }}
         >
           <label htmlFor="username">username</label>
-          <input id="username" type="text" className={style.Input}></input>
+          <input
+            id="username"
+            type="text"
+            className={style.Input}
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          ></input>
           <label htmlFor="password">password</label>
-          <input id="password" type="password" className={style.Input}></input>
+          <input
+            id="password"
+            type="password"
+            className={style.Input}
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
           <input type="submit" className={style.Submit}></input>
         </form>
       </div>
--- habit_frontend/src/components/View.tsx
+++ habit_frontend/src/components/View.tsx
@@ -1,8 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import style from "../styles/View.module.css";
 const View = () => {
+  interface habit {
+    habit: string;
+  }
+  const [habits, setHabits] = useState<habit[]>([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5656/user/${"3345345"}/habits`)
+      .then(({ data }) => {
+        console.log(data);
+        setHabits(data);
+      });
+  }, []);
   return (
     <div className={style.View}>
       <p>This is the view</p>
+      {habits.map((habit, indx) => {
+        return <p key={habit.habit + indx}> {habit.habit}</p>;
+      })}
     </div>
   );
 };

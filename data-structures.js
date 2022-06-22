00d202c9e712046fa906940b85aa580622e83786
Ellis Lee <ellismckenzielee@gmail.com>
2022-06-22 12:20:38 +0100

    initial data structures

---
+++ data-structures.js
@@ -0,0 +1,23 @@
+// user data
+const user = {
+  id: "fetru845u9845y",
+  username: "ellis",
+  password: "dfvbdfl,go4t934gg54g85gjirjbvklfd",
+  habits: ["dfgdfg45645", "dbt5646456"],
+  pairId: null,
+};
+
+// habit definition
+const habit = {
+  id: "fdvdkn58hgukfhkv34",
+  name: "sleeping",
+  type: "daily",
+};
+
+// habit data for each week
+const weekData = {
+  id: "nfdvkdlnbkltn547567",
+  habit_id: "dnfndbiontio",
+  weekCommencing: "MondayXXXX",
+  habitHistory: [0, 0, 0, 0, 0, 0, 0],
+};

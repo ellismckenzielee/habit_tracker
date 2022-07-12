1220312929b9289a71013c2f61cc9d11e90bc262
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-12 14:29:47 +0100

    update colour scheme for add, delete and popup components

--- habit_frontend/src/components/View.tsx
+++ habit_frontend/src/components/View.tsx
@@ -14,6 +14,7 @@ const View = ({ date, focus }: { date: string; focus: string }) => {
   const dates = getDatesForWeek(date);
   const totalDates = habits.length * 7;
   let count = 0;
+  let longestStreak = 0;
   useEffect(() => {
     if (focus) {
       getHabitsByUserId(focus, setHabits);
@@ -40,6 +41,7 @@ const View = ({ date, focus }: { date: string; focus: string }) => {
               <p className={style.HabitTitle}> {habit.name}</p>
               {dates.map((date, indx) => {
                 if (habit.dates.includes(date)) count++;
+                if (habit.streak > longestStreak) longestStreak = habit.streak;
                 return (
                   <div
                     key={date + indx}
@@ -47,7 +49,7 @@ const View = ({ date, focus }: { date: string; focus: string }) => {
                       habit.dates.includes(date)
                         ? style.HabitSuccess
                         : style.HabitCheckBox
-                    }`}
+                    } w-10 rounded-full ml-auto mr-auto border-2 border-black`}
                     onClick={(e) => {
                       if (date === moment().format("DD-MM-YYYY")) {
                         let action = habit.dates.includes(date)
@@ -81,12 +83,31 @@ const View = ({ date, focus }: { date: string; focus: string }) => {
           );
         })}
       </div>
-      <div className={`flex flex-row justify-center m-5`}>
-        <div className={`p-10 sm:w-50 bg-indigo-100 rounded-lg`}>
-          <p className={`text-indigo-700 font-bold`}>Week Completion:</p>
+      <div className={`flex flex-row flex-wrap justify-center m-5 gap-5`}>
+        <div
+          className={`p-2 grow basis-1/2 md:basis-1/4  bg-indigo-100 rounded-lg flex flex-col justify-center`}
+        >
+          <p className={`text-indigo-400`}>
+            <span className={"text-3xl font-bold text-indigo-900"}>
+              {((count / totalDates) * 100).toFixed(1)}
+            </span>{" "}
+            %
+          </p>
+          <p className={`text-indigo-700 font-bold m-auto`}>Week Completion</p>
+        </div>
+        <div
+          className={`p-2 grow basis-1/2 md:basis-1/4  bg-indigo-100 rounded-lg flex flex-col justify-center`}
+        >
           <p className={`text-indigo-400`}>
             {" "}
-            {((count / totalDates) * 100).toFixed(1) + "%"}
+            <span className={"text-3xl font-bold text-indigo-900"}>
+              {longestStreak}
+            </span>
+            days
+          </p>
+
+          <p className={`text-indigo-700 font-bold m-auto`}>
+            Current longest streak
           </p>
         </div>
       </div>

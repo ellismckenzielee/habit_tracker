609a5dc5405d4f65c984a308d70209257241cd92
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-14 12:08:06 +0100

    add test for checkCheckboxModifiable function: check returns true when inputs correct

--- habit_frontend/src/utils/utils.test.ts
+++ habit_frontend/src/utils/utils.test.ts
@@ -1,5 +1,7 @@
 import { checkCheckBoxModifiable } from "./utils";
 
+jest.useFakeTimers();
+jest.setSystemTime(new Date("2022-07-01T09:39:19.941Z"));
 describe("testing utility functions", () => {
   describe("testing checkCheckboxModifiable function", () => {
     test("should return a boolean", () => {
@@ -12,6 +14,17 @@ describe("testing utility functions", () => {
       const result = checkCheckBoxModifiable(date, username, displayUser);
       // ASSERT
       expect(typeof result).toBe(expected);
+    });
+    test("should return true when the current date matches the date input, and the username matches the displayUser", () => {
+      // ARRANGE
+      const expected = true;
+      const date = "01-07-2022";
+      const username = "ellismckenzielee";
+      const displayUser = "ellismckenzielee";
+      // ACT
+      const result = checkCheckBoxModifiable(date, username, displayUser);
+      // ASSERT
+      expect(result).toBe(expected);
     });
   });
 });

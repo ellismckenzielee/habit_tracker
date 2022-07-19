f3753b763d203702a50ea96c6b80a6efb410999b
Ellis Lee <ellismckenzielee@gmail.com>
2022-07-19 15:38:03 +0100

    create usePair custom hook

--- habit_frontend/src/components/Main.tsx
+++ habit_frontend/src/components/Main.tsx
@@ -1,49 +1,46 @@
 import style from "../styles/Main.module.css";
 import View from "./View";
 import { UserContext, UserContextType } from "../context/UserContext";
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import Date from "./Date";
-import { getMonday } from "../utils/date.utils";
 import Switch from "./Switch";
-import { getPairByUserId } from "../utils/utils";
+import usePair from "../hooks/usePair";
 const Main = () => {
   const { user, isLoggedIn, setPair, pair } = useContext(
     UserContext
   ) as UserContextType;
-  const [date, setDate] = useState<string>(getMonday(0));
-  const [focus, setFocus] = useState<string>(user.username);
-  console.log("FOCUS", focus);
-  console.log("PAIR", pair);
-  useEffect(() => {
-    getPairByUserId(user.username, setPair);
-  }, [user.username]);
+  const { focus, setFocus, date, setDate } = usePair(user, pair, setPair);
   return (
     <div className={style.Main}>
       {!isLoggedIn && <Navigate to="/" />}
-      <Switch
-        focus={focus}
-        status={pair.status}
-        username={user.username}
-        pairName={pair.pairId}
-        setFocus={setFocus}
-      />
-      {focus === user.username && (
-        <h1 className={`p-3 `}>
-          Here are your habits,{" "}
-          <span className="text-indigo-500">{user.username}</span>.
-        </h1>
-      )}
-      {focus !== user.username && (
-        <h1 className={`p-3 `}>
-          Here are <span className="text-indigo-500">{pair.pairId}</span>&#39;s
-          habits
-        </h1>
-      )}
+      {setFocus && (
+        <>
+          <Switch
+            focus={focus}
+            status={pair.status}
+            username={user.username}
+            pairName={pair.pairId}
+            setFocus={setFocus}
+          />
+          {focus === user.username && (
+            <h1 className={`p-3 `}>
+              Here are your habits,{" "}
+              <span className="text-indigo-500">{user.username}</span>.
+            </h1>
+          )}
+          {focus !== user.username && (
+            <h1 className={`p-3 `}>
+              Here are <span className="text-indigo-500">{pair.pairId}</span>
+              &#39;s habits
+            </h1>
+          )}
 
-      <Date date={date} setDate={setDate} />
+          <Date date={date} setDate={setDate} />
 
-      <View focus={focus} date={date} />
+          <View focus={focus} date={date} />
+        </>
+      )}
     </div>
   );
 };
